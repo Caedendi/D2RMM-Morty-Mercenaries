82e286779211e6bb53e13143fc827f28378724e8
Caedendi <Caedendi@users.noreply.github.com>
2024-03-28 00:07:19 +0100

    v1.0.0 (#1)
    - Feature parity with Morty Mercenaries by Caleb & Friends
    - Added option for custom merc name
    - Added new sound effects and text for "I can't use that" & "I can't use that yet"

---
+++ MortyMercenaries/mod.js
@@ -0,0 +1,171 @@
+//===============//
+//   Constants   //
+//===============//
+
+class FileConstants {
+  // extensions
+  static FILE_EXTENSION_JSON = ".json";
+
+  // localized jsons
+  static PATH_LOCAL_LNG_STRINGS = "local\\lng\\strings\\";
+  static FILE_VO_JSON           = `${this.PATH_LOCAL_LNG_STRINGS}vo${this.FILE_EXTENSION_JSON}`;
+  static FILE_MERCENARIES_JSON  = `${this.PATH_LOCAL_LNG_STRINGS}mercenaries${this.FILE_EXTENSION_JSON}`;
+
+  static PATH_MOD_SFX_GLOBAL = "data\\sfx_global";
+  static PATH_MOD_SFX_HD     = "data\\sfx_hd";
+  static PATH_MOD_SFX_LOCAL  = "data\\sfx_local";
+  static PATH_MOD_SPRITES    = "data\\sprites";
+
+  static PATH_GAME_SFX_GLOBAL = "global\\sfx\\monster";
+  static PATH_GAME_SFX_HD     = "hd\\global\\sfx\\monster";
+  static PATH_GAME_SFX_LOCAL  = "local\\sfx\\common\\hireables";
+  static PATH_GAME_SPRITES    = "hd\\global\\ui\\hireables";
+
+  static jsonProperties = {
+      id:   "id",
+     key:  "Key",
+    enus: "enUS",
+    zhtw: "zhTW",
+    dede: "deDE",
+    eses: "esES",
+    frfr: "frFR",
+    itit: "itIT",
+    kokr: "koKR",
+    plpl: "plPL",
+    esmx: "esMX",
+    jajp: "jaJP",
+    ptbr: "ptBR",
+    ruru: "ruRU",
+    zhcn: "zhCN",
+  };
+}
+  
+class ModConstants {
+  static mercNames = {
+    latin:    "Morty",
+    chinese:  "莫蒂",
+    korean:   "모티",
+    japanese: "モーティ",
+    cyrillic: "Морти",
+  };
+
+  static subtitles = {
+    textPotion: "Thanks.",
+    keysPotion: [
+      "female_thankyou", 
+      "male_thankyou1", 
+      "male_thankyou2", 
+      "male_thankyou3",
+    ],
+    textNewEquip: "Thanks, Rick.",
+    keysNewEquip: [
+      "female_illputthattogooduse", 
+      "male_illputthattogooduse1", 
+      "male_illputthattogooduse2", 
+      "male_illputthattogooduse3",
+    ],
+    textCantEquip: "Oh, Rick, something's not right.",
+    keysCantEquip: [
+      "female_icantusethat",
+      "female_icantusethatyet",
+      "male_icantusethat1",
+      "male_icantusethat2",
+      "male_icantusethat3",
+      "male_icantusethatyet1",
+      "male_icantusethatyet2",
+      "male_icantusethatyet3",
+    ],
+  };
+}
+
+class MortyMercenariesMod {
+  build() {
+    if (config.shouldRenameMercenary) {
+      this.renameMercenary();
+    }
+
+    if (config.shouldReplaceMercenarySprites) {
+      this.replaceSprites();
+    }
+
+    if (config.shouldReplaceMercenarySfx) {
+      this.replaceSfx();
+      this.replaceSubtitles();
+    }
+  }
+
+  renameMercenary() {
+    let path = FileConstants.FILE_MERCENARIES_JSON;
+    let file = D2RMM.readJson(path);
+
+    file.forEach(entry => {
+      let shouldUseCustomName = (config.mercenaryName != null && config.mercenaryName !== "");
+      entry[FileConstants.jsonProperties.enus] = shouldUseCustomName ? config.mercenaryName : ModConstants.mercNames.latin;
+      entry[FileConstants.jsonProperties.zhtw] = shouldUseCustomName ? config.mercenaryName : ModConstants.mercNames.chinese;
+      entry[FileConstants.jsonProperties.dede] = shouldUseCustomName ? config.mercenaryName : ModConstants.mercNames.latin;
+      entry[FileConstants.jsonProperties.eses] = shouldUseCustomName ? config.mercenaryName : ModConstants.mercNames.latin;
+      entry[FileConstants.jsonProperties.frfr] = shouldUseCustomName ? config.mercenaryName : ModConstants.mercNames.latin;
+      entry[FileConstants.jsonProperties.itit] = shouldUseCustomName ? config.mercenaryName : ModConstants.mercNames.latin;
+      entry[FileConstants.jsonProperties.kokr] = shouldUseCustomName ? config.mercenaryName : ModConstants.mercNames.korean;
+      entry[FileConstants.jsonProperties.plpl] = shouldUseCustomName ? config.mercenaryName : ModConstants.mercNames.latin;
+      entry[FileConstants.jsonProperties.esmx] = shouldUseCustomName ? config.mercenaryName : ModConstants.mercNames.latin;
+      entry[FileConstants.jsonProperties.jajp] = shouldUseCustomName ? config.mercenaryName : ModConstants.mercNames.japanese;
+      entry[FileConstants.jsonProperties.ptbr] = shouldUseCustomName ? config.mercenaryName : ModConstants.mercNames.latin;
+      entry[FileConstants.jsonProperties.ruru] = shouldUseCustomName ? config.mercenaryName : ModConstants.mercNames.cyrillic;
+      entry[FileConstants.jsonProperties.zhcn] = shouldUseCustomName ? config.mercenaryName : ModConstants.mercNames.chinese;
+    });
+    
+    D2RMM.writeJson(path, file);
+  }
+
+  replaceSprites() {
+    D2RMM.copyFile(FileConstants.PATH_MOD_SPRITES, FileConstants.PATH_GAME_SPRITES, true); // copy <mod folder>\sprites contents to <diablo 2 folder>\mods\<modname>\<modname>.mpq\data\hd\global\ui\hireables
+  }
+
+  replaceSfx() {
+    // TODO: "I can't use that (yet)" contenders:
+    // - Lawnmower Dog - Morty: Oh, man, Rick, this is pretty weird.
+    // - Meeseeks and Destroy - Morty: I can’t do it, Rick! They’re my parents and sister! 
+    // - Rick Potion #9 - Morty: Heck yeah! Thank you, Grandpa Rick! (walks to the door, but thinks about it and looks back) Hey there's no dangers or anything or side effects, right?
+    // - Rick Potion #9 - Morty: Oh, Rick, something's not right. 
+    // - The Jerrick Trap - Morty: I'm telling you, man, you're making a big mistake here. 
+    // - Rickfending Your Mort - Morty: Nope. I'm good. 
+
+    D2RMM.copyFile(FileConstants.PATH_MOD_SFX_LOCAL,  FileConstants.PATH_GAME_SFX_LOCAL,  true); // copy <mod folder>\sfx_local  contents to <diablo 2 folder>\mods\<modname>\<modname>.mpq\data\local\sfx\common\hireables
+    D2RMM.copyFile(FileConstants.PATH_MOD_SFX_HD,     FileConstants.PATH_GAME_SFX_HD,     true); // copy <mod folder>\sfx_hd     contents to <diablo 2 folder>\mods\<modname>\<modname>.mpq\data\hd\global\sfx\monster
+    D2RMM.copyFile(FileConstants.PATH_MOD_SFX_GLOBAL, FileConstants.PATH_GAME_SFX_GLOBAL, true); // copy <mod folder>\sfx_global contents to <diablo 2 folder>\mods\<modname>\<modname>.mpq\data\global\sfx\monster
+  }
+
+  replaceSubtitles() {
+    let path = FileConstants.FILE_VO_JSON;
+    let file = D2RMM.readJson(path);
+
+    file.forEach(entry => {
+      this.replaceSubtitle(entry, ModConstants.subtitles.keysPotion, ModConstants.subtitles.textPotion);
+      this.replaceSubtitle(entry, ModConstants.subtitles.keysNewEquip, ModConstants.subtitles.textNewEquip);
+      this.replaceSubtitle(entry, ModConstants.subtitles.keysCantEquip, ModConstants.subtitles.textCantEquip);
+    });
+
+    D2RMM.writeJson(path, file);
+  }
+
+  replaceSubtitle(entry, keys, newText) {
+    if (keys.includes(entry[FileConstants.jsonProperties.key])) { // if entry.Key matches with one of the entries in keys
+      entry[FileConstants.jsonProperties.enus] = newText;
+      entry[FileConstants.jsonProperties.zhtw] = newText;
+      entry[FileConstants.jsonProperties.dede] = newText;
+      entry[FileConstants.jsonProperties.eses] = newText;
+      entry[FileConstants.jsonProperties.frfr] = newText;
+      entry[FileConstants.jsonProperties.itit] = newText;
+      entry[FileConstants.jsonProperties.kokr] = newText;
+      entry[FileConstants.jsonProperties.plpl] = newText;
+      entry[FileConstants.jsonProperties.esmx] = newText;
+      entry[FileConstants.jsonProperties.jajp] = newText;
+      entry[FileConstants.jsonProperties.ptbr] = newText;
+      entry[FileConstants.jsonProperties.ruru] = newText;
+      entry[FileConstants.jsonProperties.zhcn] = newText;
+    }
+  }
+}
+
+(new MortyMercenariesMod()).build();
--- README.md
+++ README.md
@@ -1,2 +1,70 @@
-# D2RMM-Morty-Mercenaries
-D2RMM port of the Morty Mercenaries mod by Caleb and Friends
+# Morty Mercenaries for D2RMM
+
+D2RMM port of the [Morty Mercenaries](https://www.nexusmods.com/diablo2resurrected/mods/476) mod by [Caleb & Friends](https://www.nexusmods.com/diablo2resurrected/users/171503373).
+
+Renames all mercenaries to Morty, uses "Evil Morty" icon (sprite) & uses Morty's voice for dying, getting hit, giving gear & giving potions.
+
+
+## Table of Contents
+
+- [Demos](#demos)
+- [Changelog](#changelog)
+  - [1.0.0](#100)
+- [How to Install](#how-to-install)
+- [Roadmap](#roadmap)
+- [Recommended Mods](#recommended-mods)
+- [Credits](#credits)
+- [License/Permissions](#licensepermissions)
+
+
+## Demos
+
+1. [Merc potion SFX replacement](https://www.youtube.com/watch?v=EHwpkw-BLDA)
+2. [Merc equipment SFX replacement](https://www.youtube.com/watch?v=aHRrSZv4QYI)
+3. [Damage and dying SFX replacement](https://www.youtube.com/watch?v=gEywagc1m1g)
+
+
+## Changelog
+
+### 1.0.0
+
+- Feature parity with Morty Mercenaries by Caleb & Friends
+- Added option for custom merc name
+- Added new sound effects and text for _"I can't use that"_ & _"I can't use that yet"_
+
+
+## How to Install
+
+- Download and install [D2RMM](https://www.nexusmods.com/diablo2resurrected/mods/169), then run it.
+- Download and extract this mod folder to /D2RMM/mods/.
+- See D2RMM instructions on how to configure and enable.
+- Play the game!
+
+
+## Roadmap
+
+- [x] Feature parity with the original mod
+- [x] Add option for custom mercenary name
+- [x] Find and implement new sound effects for _"I can't use that"_ & _"I can't use that yet"_.
+
+
+## Recommended Mods
+
+Visit [this GitHub page](https://github.com/Caedendi/D2RMM-Mod-Lists#todo) for the list of D2RMM mods that I use, you might like some of 'em! 
+
+A list of the other mods I have created can be found [there](https://github.com/Caedendi/D2RMM-Mod-Lists#todo) too, as well as directly on [my Nexus Mods page](https://www.nexusmods.com/diablo2resurrected/users/179695179?tab=user+files).
+
+
+## Credits
+
+- [Caleb & Friends](https://www.nexusmods.com/diablo2resurrected/users/171503373) ([Discord](https://discord.gg/X8RNYUTcdR)) for creating [the original mod](https://www.nexusmods.com/diablo2resurrected/mods/476) and ripping the new sound effect
+- [olegbl](https://github.com/olegbl) for creating [D2RMM](https://www.nexusmods.com/diablo2resurrected/mods/169)
+
+
+## License/Permissions
+
+This code is licensed under GPL. 
+
+You are free to use and distribute all code in this mod, as long as you ask for permission (and permission is given), it stays open source, free of charge and all due credit is given. 
+
+If you are trying to profit off this mod in any way, then you're a dick and forbidden from using this code.
